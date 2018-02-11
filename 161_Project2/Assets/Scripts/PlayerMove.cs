15a8d009c548f934a8d793a87078f8e2de2809e8
Victoria B <vbarinov@uci.edu>
2018-02-10 20:07:03 -0800

    Player Movement - Restricted
    Can't combine movement keys, restricted in range. The range of movement
    is -3, 3 units in the worldspace.

--- 161_Project2/Assets/Scripts/PlayerMove.cs
+++ 161_Project2/Assets/Scripts/PlayerMove.cs
@@ -5,29 +5,30 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour {
 
     public GameObject Main_Camera;
+
 	// Use this for initialization
 	void Start () {
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow))
         {
             transform.Rotate(Vector3.down * 50 * Time.deltaTime);
-
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow))
         {
             transform.Rotate(Vector3.up * 50 * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.UpArrow) )
+        else if (Input.GetKey(KeyCode.UpArrow) && Main_Camera.transform.position.y <= 3)
         {
-            Main_Camera.transform.Translate(Vector3.up * 0.1f);
+            Main_Camera.transform.Translate(Vector3.up * 0.05f);
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow) && Main_Camera.transform.position.y >= -3)
         {
-            Main_Camera.transform.Translate(Vector3.down * 0.1f);
+            Main_Camera.transform.Translate(Vector3.down * 0.05f);
         }
 
     }
+
 }
