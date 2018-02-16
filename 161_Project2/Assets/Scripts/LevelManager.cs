68ac65dce7de91bf2448e888214d05a5c28fbaa9
Victoria B <vbarinova97@gmail.com>
2018-02-15 17:33:52 -0800

    🍨 Main Menu, Level Loaders, and minor fixes
    Has simple main menu now. Game over instantly sends back to start.
    Restart key works (R). Camera had weird bug where it would sometimes
    upon hit w/ floor or ceiling or enemy it would rotate a bit?? i froze
    rotation on camera since we have no need for that.

---
+++ 161_Project2/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void MoveScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public static void GameOver()
+    {
+        if (PlayerHealth.GameOver)
+        {
+            PlayerHealth.GameOver = false;
+            SceneManager.LoadScene(0);
+        }
+            
+    }
+}
